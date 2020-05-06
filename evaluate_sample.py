21f47f28a400a65a2d2d15207f541f50e1f5c365
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2020-05-06 13:20:38 +0530

    Update evaluate_sample.py

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -45,12 +45,13 @@ def main(args):
                 weights='rgb_imagenet_and_kinetics',
                 input_shape=(NUM_FRAMES, FRAME_HEIGHT, FRAME_WIDTH, NUM_RGB_CHANNELS),
                 classes=NUM_CLASSES)
-
+       
+        print(rgb_model.summary())  
         # load RGB sample (just one example)
-        rgb_sample = np.load(SAMPLE_DATA_PATH['rgb'])
+#         rgb_sample = np.load(SAMPLE_DATA_PATH['rgb'])
         
         # make prediction
-        rgb_logits = rgb_model.predict(rgb_sample)
+#         rgb_logits = rgb_model.predict(rgb_sample)
 
 
     if args.eval_type in ['flow', 'joint']:
@@ -72,30 +73,30 @@ def main(args):
                 classes=NUM_CLASSES)
             
         # load flow sample (just one example)
-        flow_sample = np.load(SAMPLE_DATA_PATH['flow'])
+#         flow_sample = np.load(SAMPLE_DATA_PATH['flow'])
         
         # make prediction
-        flow_logits = flow_model.predict(flow_sample)
-
+#         flow_logits = flow_model.predict(flow_sample)
+    
 
     # produce final model logits
-    if args.eval_type == 'rgb':
-        sample_logits = rgb_logits
-    elif args.eval_type == 'flow':
-        sample_logits = flow_logits
-    else: # joint
-        sample_logits = rgb_logits + flow_logits
+#     if args.eval_type == 'rgb':
+#         sample_logits = rgb_logits
+#     elif args.eval_type == 'flow':
+#         sample_logits = flow_logits
+#     else: # joint
+#         sample_logits = rgb_logits + flow_logits
 
-    # produce softmax output from model logit for class probabilities
-    sample_logits = sample_logits[0] # we are dealing with just one example
-    sample_predictions = np.exp(sample_logits) / np.sum(np.exp(sample_logits))
+#     # produce softmax output from model logit for class probabilities
+#     sample_logits = sample_logits[0] # we are dealing with just one example
+#     sample_predictions = np.exp(sample_logits) / np.sum(np.exp(sample_logits))
 
-    sorted_indices = np.argsort(sample_predictions)[::-1]
+#     sorted_indices = np.argsort(sample_predictions)[::-1]
 
-    print('\nNorm of logits: %f' % np.linalg.norm(sample_logits))
-    print('\nTop classes and probabilities')
-    for index in sorted_indices[:20]:
-        print(sample_predictions[index], sample_logits[index], kinetics_classes[index])
+#     print('\nNorm of logits: %f' % np.linalg.norm(sample_logits))
+#     print('\nTop classes and probabilities')
+#     for index in sorted_indices[:20]:
+#         print(sample_predictions[index], sample_logits[index], kinetics_classes[index])
 
     return 
 
