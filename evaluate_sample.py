10e4f10590db78ab502412146ca04d15decfb772
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2020-05-07 16:05:55 +0530

    Update evaluate_sample.py

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -109,7 +109,7 @@ def main(args):
 #         print(sample_predictions[index], sample_logits[index], kinetics_classes[index])
     import STLSTM
     NUM_CELL = 1
-    FILTERS = 256
+    FILTERS = 32
     KERNEL_SIZE = 3
     
     #Model 1
