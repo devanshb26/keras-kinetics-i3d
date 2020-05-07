a91cf3f97dd6994cb633228feeda58dec06447ce
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2020-05-07 13:09:25 +0530

    Update evaluate_sample.py

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -32,9 +32,6 @@ def main(args):
     
     model = ResNet50(weights='imagenet')
     model_res = Model(inputs=model.input, outputs=[model.get_layer('activation_1').output,model.get_layer('activation_7').output,model.get_layer('activation_7').output])
-    model1 = Model(inputs=model.input, outputs=model.get_layer('activation_1').output)
-    model2 = Model(inputs=model.input,outputs=model.get_layer('activation_7').output)
-    model3 = Model(inputs=model.input,outputs=model.get_layer('activation_7').output)
     
     
     # load the kinetics classes
@@ -111,7 +108,7 @@ def main(args):
 #     for index in sorted_indices[:20]:
 #         print(sample_predictions[index], sample_logits[index], kinetics_classes[index])
     import STLSTM
-    NUM_CELL = 4
+    NUM_CELL = 5
     FILTERS = 128
     KERNEL_SIZE = 3
     
@@ -121,7 +118,7 @@ def main(args):
 #     rgb=model_rgb(input1)
 #     flow=model_flow(input2)
     cells = STLSTM.StackedSTLSTMCells([STLSTM.STLSTMCell(filters=FILTERS, kernel_size=KERNEL_SIZE) for _ in range(NUM_CELL)])
-    x=STLSTM.STLSTM2D(cells, return_sequences=True)(model_rgb.output)
+    x=STLSTM.STLSTM2D(cells, return_sequences=True,input_shape=(5,28,28,64))(model_rgb.output)
     model_final=Model(inputs=model_rgb.input,outputs=x)
     print(model_final.summary())
 #     x=STLSTM(rgb+flow)
