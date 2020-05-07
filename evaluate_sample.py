254f75bca0a7ba430758445894aa0c57f5e70f47
devanshb26 <34097687+devanshb26@users.noreply.github.com>
2020-05-07 20:59:01 +0530

    Update evaluate_sample.py

--- evaluate_sample.py
+++ evaluate_sample.py
@@ -136,8 +136,8 @@ def main(args):
     x_flow1=model_flow(x_flow)
     [merge1,merge2,merge3]=model_res(x_rgb[ : ,0, : , : , : ]) 
     for i in range(1,10):
-        [m1,m2,m3]=model_res(x_rgb[i, : , : , : ])
-#         merge1=concatenate[merge1,m1]
+        [m1,m2,m3]=model_res(x_rgb[:,i, : , : , : ])
+        merge1.append(m1)
     print(merge1.shape)    
     x=STLSTM.STLSTM2D(cells0, return_sequences=True)(x+x_flow1)
     x=STLSTM.STLSTM2D(cells1, return_sequences=True)(x)
